e1998123984330c6165eeb99d688f641277644e6
willyochim <yochimw@gmail.com>
2025-03-19 00:52:24 -0400

    Demo prep

--- PostPioneer/4p02 Testing/postpioneer/mergedApp.py
+++ PostPioneer/4p02 Testing/postpioneer/mergedApp.py
@@ -646,36 +646,6 @@ def hil_submit():
 # Start the scheduler
 scheduler.start()
 
-#################################
-# Google API Implementation for Payment 
-#################################
-
-stripe.api_key = "sk_test_51QuKO2KsmLUG0fTBiVvflwSJ94bwiEHx8sTnXyCHKhFNA6JKIKmLX0y9fKVohnPlmrUP86osPlZRgsubXyMMjXlY00Op5RpSJ8"  # Replace with your actual Stripe secret key.
-
-@app.route("/process-payment", methods=["POST"])
-def process_payment():
-    data = request.get_json()
-    payment_token_str = data.get("paymentToken")
-    print(f"Receieved payment token: {payment_token_str}")
-    print(f"Request payload: {data}")
-    
-    if not payment_token_str:
-        return jsonify({"error": "Missing payment token"}), 400
-
-    try:
-        payment_token = json.loads(payment_token_str).get("id")
-
-        charge = stripe.Charge.create(
-            amount=100,  
-            currency="cad",
-            source=payment_token,
-            description="Test payment"
-        )
-        return jsonify({"status": "success", "charge": charge})
-    except Exception as e:
-        print(f"Error processing payment: {e}")
-        return jsonify({"error": str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=False)
