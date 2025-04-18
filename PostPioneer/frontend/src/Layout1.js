c533d80c983a3a54824f54730d84a0d572632c77
cg20gl <cg20gl@brocku.ca>
2025-04-18 16:50:16 -0400

    Changed Layout and Added Dashboard to Navbar
    - Now the footer contains all secondary pages, reducing navbar clutter and making the footer a secondary navbar in a sense
    - Feel free to change this if you don't like my layout

--- PostPioneer/frontend/src/Layout1.js
+++ PostPioneer/frontend/src/Layout1.js
@@ -47,13 +47,7 @@ function Layout1({ children }) {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/how-it-works">How it Works</Link>
-            </li>
-            <li>
-              <Link to="/support">Support Page</Link>
+              <Link to="/dashboard">Dashboard</Link>
             </li>
             {user ? (
               <li>
@@ -94,6 +88,19 @@ function Layout1({ children }) {
       <div className="content">{children}</div>
 
       <footer>
+        <nav>
+        <ul>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/how-it-works">How it Works</Link>
+          </li>
+          <li>
+            <Link to="/support">Contact Support</Link>
+          </li>
+        </ul>
+        </nav>
         <p>&copy; 2025 PostPioneer. All rights reserved.</p>
       </footer>
     </>
